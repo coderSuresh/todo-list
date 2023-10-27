d5a780d4a365d6b4669b191f88544d669c614afe
codersuresh <sd807141@gmail.com>
2023-10-27 15:56:52 +0545

    fix position of filter btns

--- components/ListControls.tsx
+++ components/ListControls.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const ListControls = () => {
     return (
-        <div className='flex px-5 py-2 justify-between gap-5 items-center text-grayish-text'>
+        <div className='flex px-5 py-2 justify-between items-center text-sm text-grayish-text'>
 
             <div>
                 <p>5 items left</p>
             </div>
 
-            <div className='flex gap-3 font-bold md:static absolute -bottom-14 bg-card-bg py-2 px-5 w-full justify-center'>
+            <div className='flex md:w-fit w-full gap-3 font-bold md:static absolute -bottom-16 left-0 right-0 rounded-md bg-card-bg py-2 px-5 justify-center'>
                 <button className='text-bright-blue hover:text-text-hovered'>All</button>
                 <button className='hover:text-text-hovered'>Active</button>
                 <button className='hover:text-text-hovered'>Completed</button>
