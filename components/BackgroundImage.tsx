03ff6d16e0c1b7df0249cc0cda1a5d750518976b
codersuresh <sd807141@gmail.com>
2023-10-28 21:50:31 +0545

    fix: use hooks to change bg img dynamically

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -6,11 +6,19 @@ const BackgroundImage = () => {
 
     const { theme } = React.useContext(ThemeContext)
 
-    const bgImg = `md:bg-desktop-${theme} bg-mobile-${theme}`
+    const [bgImg, setBgImg] = React.useState('bg-mobile-light')
+
+    React.useEffect(() => {
+        if (theme === 'dark') {
+            setBgImg('bg-mobile-dark')
+        } else {
+            setBgImg('bg-mobile-light')
+        }
+    }, [theme])
 
     return (
         <div
-            className={`fixed top-0 left-0 right-0 z-0 ${bgImg} 
+            className={`fixed ${bgImg} 
                 bg-no-repeat bg-cover w-full 
                 md:h-72 h-60`}
         />
