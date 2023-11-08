b20ab35b028566214c64887b93381610dfd547f2
codersuresh <sd807141@gmail.com>
2023-11-08 16:11:15 +0545

    add sw

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
     const withPWA = require('next-pwa')({
       dest: 'public',
       register: true,
+      sw: '/sw.js',
       skipWaiting: true,
       disable: process.env.NODE_ENV === 'development'
     })
