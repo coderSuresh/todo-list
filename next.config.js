ee6b6c26ce928769a98654f482bf2ad197608f25
codersuresh <sd807141@gmail.com>
2023-11-08 22:28:38 +0545

    Add buildExcludes to withPWA configuration.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,26 @@
 /** @type {import('next').NextConfig} */
 
-    const withPWA = require('next-pwa')({
-      dest: 'public',
-      register: true,
-      sw: '/sw.js',
-      skipWaiting: true,
-      disable: process.env.NODE_ENV === 'development'
-    })
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  sw: '/sw.js',
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  exclude: [
+    ({ asset, compilation }) => {
+      if (
+        asset.name.startsWith("server/") ||
+        asset.name.match(/^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/)
+      ) {
+        return true;
+      }
+      if (isDev && !asset.name.startsWith("static/runtime/")) {
+        return true;
+      }
+      return false;
+    }
+  ],
+})
 
 const nextConfig = withPWA({})
 
